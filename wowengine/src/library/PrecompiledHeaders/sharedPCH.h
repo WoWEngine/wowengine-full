7267de13ce7d617013e33c092bf631b789846778
Yehonal <hw.2@hotmail.it>
2015-09-11 18:24:15 +0200

    starting to fix cmake files and structure after trinity sync

--- wowengine/src/library/PrecompiledHeaders/sharedPCH.h
+++ wowengine/src/library/PrecompiledHeaders/sharedPCH.h
@@ -1,4 +1,10 @@
 //add here most rarely modified headers to speed up debug build compilation
 
-#include "TypeList.h"
-#include "GitRevision.h"
+#include "BoundingIntervalHierarchy.h"
+#include "BoundingIntervalHierarchyWrapper.h"
+#include "RegularGrid.h"
+#include "Collision/VMapDefinitions.h"
+#include "Collision/Maps/MapTree.h"
+#include "Collision/Models/WorldModel.h"
+#include "Collision/Models/ModelInstance.h"
+#include "Collision/Models/GameObjectModel.h"
